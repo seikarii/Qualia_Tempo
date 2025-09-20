43aa7f1c4db400eb908a41fcf4666a3380b501cb
seikarii <karlosseika@gmail.com>
2025-09-20 02:12:03 +0200

    s

--- start_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /media/seikarii/Nvme/QualiaTempo/qualia-tempo-prototype
-
-source /media/seikarii/Nvme/QualiaTempo/.venv/bin/activate
-
-python3 backend/main.py
--- test_websocket.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /media/seikarii/Nvme/QualiaTempo
-source .venv/bin/activate
-python3 test_websocket.py
